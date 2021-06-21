996ee4b62907edd2b4cb48599b565deb30177d27
Kevin Andres Cárdenas <kevanandres@hotmail.com>
2021-06-20 21:53:55 -0500

    Version 1

---
+++ app/src/main/java/com/tit/oxigenapp/AlarmReceiver.java
@@ -0,0 +1,14 @@
+package com.tit.oxigenapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        System.out.println("15");
+        Log.d("TAG","15");
+    }
+}
